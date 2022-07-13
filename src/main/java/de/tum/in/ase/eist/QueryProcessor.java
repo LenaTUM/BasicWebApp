b1ce6099b40c29babb01e738ddb0de822f78b09f
LZeller <lena.zeller@tum.de>
2022-07-13 15:26:06 +0200

    Added plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -7,12 +7,28 @@ public class QueryProcessor {
 
     public String process(String query) {
 		query = query.toLowerCase();
+        // String query = "what is 12 plus 13"
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "lena";
+        } else if (query.contains("plus")) {
+            //Here we get a list of words
+            String[] words = query.split(" ");
+            int sum = 0;
+            for (String word : words) {
+                try {
+                    int number = Integer.parseInt(word);
+                    sum += number;
+                } catch(Exception e) {
+                    //
+                }
+            }
+            return String.valueOf(sum);
+        } else if (query.contains("largest")) {
+            return "MyTeam";
         } else { // TODO extend the programm here
             return "";
         }
